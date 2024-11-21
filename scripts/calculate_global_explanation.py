06142ccb2b3408969fb9f44a47c058b0964f1bbb
Mikolaj Piorczynski <m.piorczynski13@gmail.com>
2024-11-21 23:11:33 +0100

    Add reading from .h5

--- scripts/calculate_global_explanation.py
+++ scripts/calculate_global_explanation.py
@@ -2,3 +2,18 @@
 # TODO: calculate ground truth (all samples) global explanation
 # TODO: calculate compressed global explanation for some sample selection method
 # TODO: compare both global explanations (MAE or TV)
+
+
+import h5py
+
+with h5py.File("explanations.h5", "r") as f:
+	for sample_idx, grp in f.items():
+		print(grp.keys())
+		
+		print(type(grp["label_idx"][()]))
+		print(type(grp["predicted_label_idx"][()]))
+		print(type(grp["explanation_fit"][()]))
+		print(type(grp["probabilities"][:]))
+		print(type(grp["token_ids"][:]))
+		print(type(grp["token_scores"][:]))
+		break

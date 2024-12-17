b49472f9b22a9ad7191bd0aeba623b3811daf929
Kamil Ciebiera <kamil.ciebiera@gmail.com>
2024-12-17 16:15:09 +0100

    Add max_variation_ratio sampling

---
+++ samplers/max_variation_ratio.py
@@ -0,0 +1,17 @@
+import numpy as np
+import h5py
+
+
+def select_samples(file: h5py.File, num_samples: int, seed: int, **kwargs) -> list:
+    # https://arxiv.org/pdf/1703.02910 maximize 1 - max(p(x))
+    id_with_variation_ratio = []
+
+    for key, value in file.items():
+        probabilities = value["probabilities"][:]
+        variation_ratio = 1 - np.max(probabilities)
+        id_with_variation_ratio.append((key, variation_ratio))
+
+    id_with_variation_ratio = sorted(
+        id_with_variation_ratio, key=lambda x: x[1], reverse=True
+    )
+    return [x[0] for x in id_with_variation_ratio[:num_samples]]
--- scripts/config.py
+++ scripts/config.py
@@ -1,4 +1,4 @@
-from samplers import uniform_sampler, entropy_sampler
+from samplers import max_variation_ratio, uniform_sampler, entropy_sampler
 
 from aggregators import norm_lime
 
@@ -6,6 +6,7 @@ from aggregators import norm_lime
 NAME_TO_SAMPLER = {
     "uniform": uniform_sampler.select_samples,
     "entropy": entropy_sampler.select_samples,
+    "variation_ratio": max_variation_ratio.select_samples,
 }
 
 NAME_TO_AGGREGATOR = {"norm_lime": norm_lime.aggregate_local_explanations}

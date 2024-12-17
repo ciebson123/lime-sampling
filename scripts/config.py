9df1c557baa87aea6ff29c670ac7b9dcff3cbf4f
Kamil Ciebiera <kamil.ciebiera@gmail.com>
2024-12-17 16:17:15 +0100

    Add el2n score sampling

--- scripts/config.py
+++ scripts/config.py
@@ -1,11 +1,11 @@
-from samplers import uniform_sampler, entropy_sampler
-
+from samplers import uniform_sampler, entropy_sampler, el2n_score
 from aggregators import norm_lime
 
 
 NAME_TO_SAMPLER = {
     "uniform": uniform_sampler.select_samples,
     "entropy": entropy_sampler.select_samples,
+    "el2n": el2n_score.select_samples,
 }
 
 NAME_TO_AGGREGATOR = {"norm_lime": norm_lime.aggregate_local_explanations}

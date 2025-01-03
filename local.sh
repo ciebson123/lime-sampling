cecdeccd197a40391eb54902633e733f8683cd75
Mikolaj Piorczynski <m.piorczynski13@gmail.com>
2025-01-03 15:25:18 +0100

    Add scripts to run all

---
+++ local.sh
@@ -0,0 +1,20 @@
+dataset=emotion
+num_samples=5000
+
+for num_features in 10 all; do
+    if [ $num_features = "all" ]; then
+        num_features_arg=""
+    else
+        num_features_arg="--lime_num_features $num_features"
+    fi
+
+    for token_masking_strategy in remove; do
+        sbatch run.sh scripts/calculate_local_explanations.py \
+            --dataset $dataset \
+            --experiment_dir "experiments/${dataset}/lime/k_${num_features}-n_${num_samples}-mask_${token_masking_strategy}" \
+            --batch_size 1024 \
+            --lime_num_samples $num_samples \
+            --lime_token_masking_strategy $token_masking_strategy \
+            $num_features_arg
+    done
+done
--- run_sampler.sh
+++
@@ -1,7 +0,0 @@
-export PYTHONPATH="${PYTHONPATH}:$(pwd)"
-
-for i in 10 20 50 100 200 500 1000; 
-    do 
-    python scripts/calculate_global_explanation.py --explanation_file storage/emotion_lime/lime/k_all-n_5000-mask_remove/explanations.h5 --num_samples $i --output_file storage/ent_exp/entropy_explanations_$i.json --sampler entropy ; 
-    echo "$i done";
-    done

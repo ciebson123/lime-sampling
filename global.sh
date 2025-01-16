b2afbe02a5aa5b3edf8d65c23b12ea3d540e1cfb
Kamil Ciebiera <kamil.ciebiera@gmail.com>
2025-01-16 20:53:20 +0100

    Added wrapper for dividing samples among classes

--- global.sh
+++ global.sh
@@ -15,14 +15,18 @@ for aggregator in norm_lime; do
     for num_samples in 10 20 50 100 200 500 1000; do 
     # for num_samples in 10 20 50 100 200 500 1000 2000 5000 10000; do 
         for sampler in uniform entropy el2n variation_ratio; do
-            echo "Running sampler $sampler with $num_samples samples"
-            bash run.sh scripts/calculate_global_explanation.py \
-                --explanation_file $experiment_dir/explanations.h5 \
-                --dataset $dataset \
-                --num_samples $num_samples \
-                --sampler $sampler \
-                --aggregator  $aggregator \
-                --output_file $experiment_dir/global_explanation-sampler_${sampler}-n_${num_samples}-aggregator_${aggregator}.json
+            # I just test whether it is set and not empty
+            for use_per_class_wrapper in "set" ""; do
+                echo "Running sampler $sampler with $num_samples samples"
+                bash run.sh scripts/calculate_global_explanation.py \
+                    --explanation_file $experiment_dir/explanations.h5 \
+                    --dataset $dataset \
+                    --num_samples $num_samples \
+                    --sampler $sampler \
+                    --aggregator  $aggregator \
+                    --output_file $experiment_dir/global_explanation-sampler_${sampler}${use_per_class_wrapper:+"_per_class"}-n_${num_samples}-aggregator_${aggregator}.json \
+                    ${use_per_class_wrapper:+"--use_per_class_wrapper"}
+            done
         done
     done    
 done

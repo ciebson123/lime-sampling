54b24bb57d571bedffaf9606d966e6c713e8908b
Janek <jan.malasnicki@gmail.com>
2025-01-20 14:54:44 +0100

    reverted run.sh changes

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=lime-sampling
-#SBATCH --partition=a100
+#SBATCH --account=mi2lab-normal
+#SBATCH --partition=short
 #SBATCH --time=1-00:00:00
+#SBATCH --constraint=dgx 
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=6GB
@@ -11,8 +13,8 @@ set -e
 hostname; pwd; date
 
 source .env
-# module load anaconda/4.0
-# source $CONDA_SOURCE
+module load anaconda/4.0
+source $CONDA_SOURCE
 eval "$(conda shell.bash hook)"
 conda activate lime-sampling
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"

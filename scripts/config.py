3601e54d46d2d7d1437b2be9965c09e361af2b06
Kamil Ciebiera <kamil.ciebiera@gmail.com>
2025-01-21 16:09:59 +0100

    Add yelp

--- scripts/config.py
+++ scripts/config.py
@@ -1,16 +1,18 @@
 from aggregators import norm_lime
-from data import emotion as emotion_data, imdb as imdb_data
-from models import emotion as emotion_models, imdb as imdb_models
+from data import emotion as emotion_data, imdb as imdb_data, yelp as yelp_data
+from models import emotion as emotion_models, imdb as imdb_models, yelp as yelp_models
 from samplers import el2n_score, entropy_sampler, max_variation_ratio, uniform_sampler
 
 NAME_TO_MODEL_LOADER = {
     "emotion": emotion_models.load_model,
     "imdb": imdb_models.load_model,
+    "yelp": yelp_models.load_model,
 }
 
 NAME_TO_DATASET_LOADER = {
     "emotion": emotion_data.load_dataset,
     "imdb": imdb_data.load_dataset,
+    "yelp": yelp_data.load_dataset,
 }
 
 NAME_TO_SAMPLER = {
